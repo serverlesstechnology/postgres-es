59ede8152ed10da135b86558f92e07a2d02d39bc
Dave Garred <dgarred@starbucks.com>
2020-04-27 20:20:43 -0700

    update query interface

--- src/cqrs.rs
+++ src/cqrs.rs
@@ -1,5 +1,3 @@
-use std::rc::Rc;
-
 use cqrs_es::{Aggregate, CqrsFramework, DomainEvent, TimeMetadataSupplier, QueryProcessor};
 use postgres::Connection;
 
@@ -10,7 +8,7 @@ use crate::PostgresStore;
 pub type PostgresCqrs<A, E> = CqrsFramework<A, E, PostgresStore<A, E>, TimeMetadataSupplier>;
 
 /// A convenience function for creating a CqrsFramework
-pub fn postgres_cqrs<A, E>(conn: Connection, query_processor: Rc<dyn QueryProcessor<A, E>>) -> PostgresCqrs<A, E>
+pub fn postgres_cqrs<A, E>(conn: Connection, query_processor: Vec<Box<dyn QueryProcessor<A, E>>>) -> PostgresCqrs<A, E>
     where A: Aggregate,
           E: DomainEvent<A>
 {
