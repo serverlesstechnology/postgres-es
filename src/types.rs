e0469154dd499189120ab0e25df8a1cf670b1db1
Dave Garred <dave.garred@gmail.com>
2025-11-15 15:33:11 -0600

    Move library to main cqrs repo

--- src/types.rs
+++
@@ -1,7 +0,0 @@
-use crate::PostgresEventRepository;
-use cqrs_es::persist::PersistedEventStore;
-use cqrs_es::CqrsFramework;
-
-/// A convenience type for a CqrsFramework backed by
-/// [PostgresStore](struct.PostgresStore.html).
-pub type PostgresCqrs<A> = CqrsFramework<A, PersistedEventStore<PostgresEventRepository, A>>;
